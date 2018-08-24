816132f05b675513f5491a953285a6c3743420b3
Vincent Stakenburg <vincentstakenburg@gmail.com>
2018-08-24 21:27:11 +0200

    added hw accel

--- src/ffmpeg.rs
+++ src/ffmpeg.rs
@@ -1,9 +1,13 @@
-use std::str::FromStr;
+extern crate itertools;
 
-#[derive(Debug)]
+use std::str::FromStr;
+use std::process::Command;
+use itertools::Itertools;
+use std::ops::DerefMut;
+
+#[derive(Debug, AsRefStr, PartialEq)]
 pub enum ResultType {
     MP4,
-    GIF,
     JPG,
 }
 
@@ -13,7 +17,6 @@ impl FromStr for ResultType {
     fn from_str(s: &str) -> Result<ResultType, ()> {
         match s.to_lowercase().as_ref() {
             "mp4" => Ok(ResultType::MP4),
-            "gif" => Ok(ResultType::GIF),
             "jpg" => Ok(ResultType::JPG),
             _ => Err(())
         }
@@ -26,6 +29,7 @@ pub struct FfmpegBuilder {
     size_y: Option<u32>,
     pos_x: Option<u32>,
     pos_y: Option<u32>,
+    record_audio: bool,
 }
 
 impl FfmpegBuilder {
@@ -36,12 +40,28 @@ impl FfmpegBuilder {
             size_y: None,
             pos_x: None,
             pos_y: None,
+            record_audio: false,
         }
     }
 
     pub fn set_result_type(self, result_type: ResultType) -> Self {
         Self {
             result_type: Some(result_type),
+            ..self
+        }
+    }
+
+    pub fn set_position(self, pos_x: u32, pos_y: u32) -> Self {
+        Self {
+            pos_x: Some(pos_x),
+            pos_y: Some(pos_y),
+            ..self
+        }
+    }
+
+    pub fn set_record_audio(self, record_audio: bool) -> Self {
+        Self {
+            record_audio,
             ..self
         }
     }
@@ -53,4 +73,82 @@ impl FfmpegBuilder {
             ..self
         }
     }
+
+    pub fn build(mut self) -> Command {
+        if self.result_type.is_none() || self.size_y.is_none() || self.size_x.is_none() || self.pos_x.is_none() || self.pos_y.is_none() {
+            panic!("FfmpegBuilder is incomplete")
+        }
+
+        let mut cmd = Command::new("ffmpeg");
+
+        let (result_type, size_x, size_y, pos_x, pos_y) = (
+            self.result_type.unwrap(),
+            self.size_x.unwrap(),
+            self.size_y.unwrap(),
+            self.pos_x.unwrap(),
+            self.pos_y.unwrap()
+        );
+
+        if result_type == ResultType::MP4 {
+            cmd.args(&[
+                "-hwaccel",
+                "vaapi",
+                "-vaapi_device",
+                "/dev/dri/renderD128",
+            ]);
+
+            cmd.args(&[
+                "-video_size",
+                [size_x, size_y].iter().join("x").as_ref(),
+                "-framerate",
+                "60"
+            ]);
+
+            cmd
+                .args(&[
+                    "-f",
+                    "x11grab"
+                ])
+                .args(&[
+                    "-i",
+                    (":0.0+".to_owned() + ([pos_x, pos_y].iter().join(",").as_ref())).as_ref()
+                ]);
+
+            if self.record_audio {
+                // TODO: don't hardcode input device and interface nr
+                cmd.args(&[
+                    "-f",
+                    "alsa",
+                    "-ac",
+                    "2",
+                    "-i",
+                    "hw:1",
+                    "-acodec",
+                    "copy"
+                ]);
+            }
+
+            cmd.args(&[
+                "-vf",
+                "format=nv12,hwupload",
+                "-bit_rate",
+                "320k",
+                "-c:v",
+                "h264_vaapi",
+            ]);
+        }
+
+
+        // finally specify output file
+        cmd.args(&[
+            "-y",
+            "-bf",
+            "0",
+            "temp.mkv"
+//            ("temp.".to_owned() + result_type.as_ref()).as_ref()
+        ]);
+
+        cmd
+    }
+
 }
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,18 @@
+#![allow(dead_code)]
+extern crate itertools;
 extern crate clap;
-use clap::{App};
-use clap::Arg;
+extern crate strum;
+#[macro_use]
+extern crate strum_macros;
+
 mod ffmpeg;
+
+use itertools::Itertools;
+use clap::{App, Arg};
+use ffmpeg::{FfmpegBuilder};
+use std::fmt::Debug;
+
+
 
 fn main() {
     let matches = App::new("Screentool")
@@ -18,12 +29,42 @@ fn main() {
                 .case_insensitive(true)
                 .default_value("jpg")
         )
+        .arg(
+            Arg::with_name("position")
+                .short("p")
+                .long("position")
+                .value_name("POSITION")
+                .help("Position of screengrab")
+                .takes_value(true)
+                .default_value("0,0")
+                .required(true)
+        )
+        .arg(
+            Arg::with_name("size")
+                .short("s")
+                .long("size")
+                .value_name("SIZE")
+                .help("Size of screengrab")
+                .takes_value(true)
+                .required(true)
+        )
         .get_matches();
 
-    let builder = ffmpeg::FfmpegBuilder::new();
-    builder.set_result_type(
-        matches.value_of("type").unwrap().parse::<ffmpeg::ResultType>().unwrap()
-    );
+    let position_iterator = matches.value_of("position").unwrap().split(",");
+    let size_iterator = matches.value_of("size").unwrap().split(",");
+    if position_iterator.clone().count() != 2 || size_iterator.clone().count() != 2 {
+        panic!();
+    }
 
+    let (pos_x, pos_y, size_x, size_y) = position_iterator.merge(size_iterator)
+        .map(|n: &str| n.parse::<u32>().unwrap())
+        .collect_tuple()
+        .unwrap();
+    let result_type = matches.value_of("type").unwrap().parse().unwrap();
+
+    println!("{:?}", FfmpegBuilder::new()
+        .set_result_type(result_type)
+        .set_position(pos_x, pos_y)
+        .set_size(size_x, size_y)
+        .build().output());
 }
-
